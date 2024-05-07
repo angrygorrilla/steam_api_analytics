d3990f5efc01b7da928def3667dfdd1572d6af0e
gchapm02@mail.uoguelph.ca <gchapm02@mail.uoguelph.ca>
2024-05-06 21:41:29 -0400

    test api calls are in place

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # steam_api_analytics
 George Testing analytics on the steam API
+
+
+# Getting started
+settup a steam API key using these instructions:
+https://steamcommunity.com/dev
+
+Then copy your api key into a file called 'steam_api.txt'
+## Environment
+  Setup anaconda and import the env.yml
+  This will install the modules this project requires
+## jupyter 
+   
+
+
+# Running
+   Run Data_collection.ipnb to see your statistics
---
+++ helpers.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import yaml
+
+def get_secrets():
+    with open("steam_api.yml", 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+    return data_loaded
+    
+    f = open("steam_api.txt", "r")
+    return f.read()
+
+def get_data(api,params=None):
+    response = requests.get(api,params)
+    if response.status_code == 200:
+        print("Successfully fetched the data")
+        return(response.json())
+    else:
+        print(f"Error: {response.status_code}. Failed to fetch data.")
+        print("Response content:", response.content)
+
+def formatted_print(self, obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
